86293b379f8c6aa7899eae1d31f40347d0a54a29
dima55279 <dima55279@yandex.ru>
2025-10-06 22:04:36 +0300

    Выполнено задание 15.

--- 15/src/Home.jsx
+++ 15/src/Home.jsx
@@ -5,9 +5,18 @@ import ThemeContext from './contexts';
 const content = 'Текст для вкладки Home';
 
 class Home extends React.Component {
-  // BEGIN (write your solution here)
+    // BEGIN (write your solution here)
+    static contextType = ThemeContext;
 
-  // END
+    render() {
+        const { currentTheme } = this.context;
+        return (
+            <article className={currentTheme.className}>
+                {content}
+            </article>
+        );
+    }
+    // END
 }
 
 export default Home;
--- 15/src/Profile.jsx
+++ 15/src/Profile.jsx
@@ -5,9 +5,18 @@ import ThemeContext from './contexts';
 const content = 'Текст для вкладки Profile';
 
 class Profile extends React.Component {
-  // BEGIN (write your solution here)
+    // BEGIN (write your solution here)
+    static contextType = ThemeContext;
 
-  // END
+    render() {
+        const { currentTheme } = this.context;
+        return (
+            <article className={currentTheme.className}>
+                {content}
+            </article>
+        );
+    }
+    // END
 }
 
 export default Profile;
